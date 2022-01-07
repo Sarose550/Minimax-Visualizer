e873983103134de6cfc64a9446fb11a71f492ff8
Sarose550 <sarose550@gmail.com>
2022-01-07 01:23:33 -0500

    added SML to tree

--- utilities.js
+++ utilities.js
@@ -209,14 +209,48 @@ function toTreeCode(node){
   return treeStr
 }
 
+function smlInt(x){
+  if(x >= 0){
+    return "" + x;
+  }
+  return "~" + (-x);
+}
+
+function parseSMLInt(x){
+  if(x.at(0) == "~"){
+    return -parseFloat(x.slice(1));
+  }
+  return parseFloat(x);
+}
+
+function isSMLNumber(str){
+  if(str == ""){
+    return false;
+  }
+  if(str.at(0) == "~"){
+    if(str.length == 1){
+      return false;
+    }
+    if(str.at(1) == "-"){
+      return false;
+    }
+    return !isNaN(Number.parseInt(str.slice(1)));
+  }
+  if(str.at(0) == "-"){
+
+    return false;
+  }
+  return !isNaN(Number.parseInt(str));
+}
+
 function toExplicitTree(node,str="a"){
   if(node.type == LEAF){
-    return 'Esti ("' + str + '", ' + node.val + ')';
+    return 'Esti("' + str + '",' + smlInt(node.val) + ')';
   }
-  var treeStr = 'Node ("' + str + '", [';
+  var treeStr = 'Node("' + str + '",[';
   for(var i = 0; i < node.children.length; i++){
     if(i != 0){
-      treeStr += ", ";
+      treeStr += ",";
     }
     treeStr += toExplicitTree(node.children[i], str + ">" + i);
   }
@@ -234,6 +268,18 @@ function loadTreeCode(){
   hideButtons();
   $(nodeOptions).hide();
   drawTreeCode(treeStr);
+}
+
+function loadExplicitTree(){
+  var treeStr = prompt("Type a tree encoding:");
+
+  if (treeStr == null) {
+    return;
+  }
+  isEditing = true;
+  hideButtons();
+  $(nodeOptions).hide();
+  drawExplicitTreeCode(treeStr);
 }
 
 function drawTreeCode(treeStr){
@@ -256,6 +302,200 @@ function drawTreeCode(treeStr){
   else{
     alert("Not a valid tree.");
   }
+}
+
+function removeUnquotedWhiteSpace(str){
+  var strArray = str.split("");
+  var s = removeUnquotedWhiteSpace1(strArray);
+  if(s == null){
+    return null;
+  }
+  return s.join("");
+}
+
+function removeUnquotedWhiteSpace1(strArray,idx=0,quoteMode=0){
+  //0 indicates that it's not inside quotes
+  //1 indicates that it's inside single quotes
+  //2 indicates that it's inside double quotes
+  //if the quotes aren't matched up, this returns null.
+  switch(quoteMode){
+    case 0:
+      if(idx == strArray.length){
+        return strArray;
+      }
+      if(strArray[idx] == "'"){
+        return removeUnquotedWhiteSpace1(strArray,idx+1,1);
+      }
+      if(strArray[idx] == '"'){
+        return removeUnquotedWhiteSpace1(strArray,idx+1,2);
+      }
+      if(/\s/.test(strArray[idx])){
+        strArray[idx] = "";
+      }
+      return removeUnquotedWhiteSpace1(strArray,idx+1,0);
+      break;
+
+    case 1:
+      if(idx == strArray.length){
+        return null;
+      }
+      if(strArray[idx] == "'"){
+        return removeUnquotedWhiteSpace1(strArray,idx+1,0);
+      }
+      return removeUnquotedWhiteSpace1(strArray,idx+1,1);
+      break;
+
+    case 2:
+      if(idx == strArray.length){
+        return null;
+      }
+      if(strArray[idx] == '"'){
+        return removeUnquotedWhiteSpace1(strArray,idx+1,0);
+      }
+      return removeUnquotedWhiteSpace1(strArray,idx+1,2);
+      break;
+  }
+}
+
+function parseArgs(args){
+  if(args.length < 2){
+    return null;
+  }
+  var c = args.at(0);
+  if(c != "'" && c != '"'){
+    return null;
+  }
+  var i = 1;
+  var closedQuotes = false;
+  while(i < args.length && !closedQuotes){
+    if(args.at(i) == c){
+      closedQuotes = true;
+    }
+    i++;
+  }
+  //console.log("Found the quoted part:");
+  //console.log(args.slice(0,i));
+  if(!closedQuotes || i == args.length){
+    return null;
+  }
+  if(args.at(i) != ","){
+    return null;
+  }
+  return args.slice(i+1);
+}
+
+function drawExplicitTreeCode(treeStr){
+  var treeFeedback = loadExplicitTreeCodeHelper(removeUnquotedWhiteSpace(treeStr),0);
+  var tree = treeFeedback[0];
+  var newNumLeaves = treeFeedback[1];
+  var max_y = treeFeedback[2];
+  if(tree != null){
+    root.children = new Array();
+    root = tree;
+    numLeaves = newNumLeaves;
+    if(max_y > canvasHeight){
+      canvasHeight = max_y + levelSpace;
+      var c = document.getElementById("drawing");
+      c.height = canvasHeight;
+    }
+    positionNodes(root,numLeaves);
+    drawAll("drawing");
+  }
+  else{
+    alert("Not a valid explicit game tree.");
+  }
+}
+
+function loadExplicitTreeCodeHelper(treeStr,level){
+  var firstToken = treeStr.slice(0,5);
+  if(treeStr == null || !(firstToken == "Esti(" || firstToken == "Node(") || treeStr.at(-1) != ")"){
+    //console.log("unmatched quote or string doesn't start with a constructor");
+    return [null,null,null];
+  }
+  //This catches unmatched quotes, or string doesn't start with one of the tokens
+  var args = treeStr.slice(5,-1);
+  //console.log("args:");
+  //console.log(args);
+  var parsedArgs = parseArgs(args);
+  //console.log("parsedArgs:");
+  //console.log(parsedArgs);
+  if(parsedArgs == null){
+    //console.log("invalid arguments to constructor");
+    return [null, null, null];
+  }
+  var tree = new Node();
+  tree.y = levelSpace/4 + levelSpace * level;
+  if(firstToken == "Esti("){
+    if(isSMLNumber(parsedArgs)){
+      tree.type = LEAF;
+      tree.val = parseSMLInt(parsedArgs);
+      return [tree,1,tree.y];
+    }
+    else{
+      //console.log("second argument of esti constructor was not an integer");
+      return [null,null,null];
+    }
+  }
+  else{
+    tree.type = 1-(level%2);
+    tree.val = null;
+  }
+  //since we're not an esti node, check that parsedArgs is a list
+  if(parsedArgs.length == 0 || !(parsedArgs.at(0) == "[" && parsedArgs.at(-1) == "]")){
+    //console.log("second argument of node constructor was not a list");
+    return [null,null,null];
+  }
+  var strBody = parsedArgs.slice(1,-1);
+  var subTrees = new Array();
+  var parenDepth = 0;
+  var start = 0;
+  var end = 0;
+  for(var i = 0; i < strBody.length; i++){
+    if(i != 0 && parenDepth == 0 && strBody.charAt(i) != "," && strBody.charAt(i-1) == ")"){
+      //console.log("expected a , to separate list entries but didn't find one");
+      return [null,null,null];
+    }
+    end++;
+    if(strBody.charAt(i) == "("){
+      parenDepth++;
+    }
+    else if(strBody.charAt(i) == ")"){
+      parenDepth--;
+      if(parenDepth == 0){
+        subTrees.push(strBody.slice(start,end));
+        start = end;
+      }
+    }
+    else if(parenDepth == 0 && strBody.charAt(i) == ","){
+      start++;
+    }
+    if(parenDepth < 0){
+      //console.log("unmatched parentheses on the left");
+      return [null,null,null];
+    }
+  }
+  if(parenDepth > 0 || subTrees.length == 0){
+    //console.log("unmatched parentheses on the right, or nonleaf had no children");
+    return [null,null,null];
+  }
+  var totalLeaves = 0;
+  var max_y = tree.y;
+  //console.log("subtrees:");
+  for(var i = 0; i < subTrees.length; i++){
+    var candidateFeedback = loadExplicitTreeCodeHelper(subTrees[i],level+1);
+    var candidate = candidateFeedback[0];
+    if(candidate == null){
+      return [null,null,null];
+    }
+    tree.children.push(candidate);
+    candidate.parent = tree;
+    totalLeaves += candidateFeedback[1];
+    max_y = Math.max(max_y,candidateFeedback[2]);
+  }
+  for(var i = 0; i < tree.children.length; i++){
+    tree.alphabetas.push(null);
+  }
+  return [tree,totalLeaves,max_y];
 }
 
 function loadTreeCodeHelper(treeStr,level){
